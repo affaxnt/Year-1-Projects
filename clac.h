417d38847b3424ae729935664ba084b6ef9f7b25
affaxnt <affan.tham@gmail.com>
2024-10-21 15:24:22 -0400

    clac.h

--- clac.h
+++ clac.h
@@ -1,7 +1,7 @@
 #ifndef CALC_H
 #define CALC_H
 #include "student.h"
-
+ 
 // Function prototypes for calculations
 void calculateGrade(Student *student);
 void recalculateAllGrades(Student students[], int numStudents);
