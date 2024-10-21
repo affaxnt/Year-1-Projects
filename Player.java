0611404d0f132f0e9d528af005c2d94efc8fb4ec
affaxnt <affan.tham@gmail.com>
2024-10-21 15:11:01 -0400

    Player.java

---
+++ Player.java
@@ -0,0 +1,17 @@
+public class Player {
+    private String name;
+    private char colour;
+
+    public Player(String name, char colour) {
+        this.name = name;
+        this.colour = colour;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public char getColour() {
+        return colour;
+    }
+}
